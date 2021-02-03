91e00f63c9797fc9fb53e11fab80dd95d37c5e1c
helman101 <helmanandresortegon@gmail.com>
2021-02-03 12:11:20 +0100

    fix User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    if @user.save?
+    if @user.save
       redirect_to @user
     else
       render :new
